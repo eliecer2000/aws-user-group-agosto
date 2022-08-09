7ae369c0b1cc24252de9c8f3af24b9bce1548e0f
Eliezer Rangel <eliezer.rangel@escala24x7.com>
2022-08-09 11:47:03 -0500

    Ajustes en SPA

--- spa/src/pages/IndexPage.vue
+++ spa/src/pages/IndexPage.vue
@@ -1,17 +1,21 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="row">
+      <div class="col-12">
+        <img
+          alt="Quasar logo"
+          src="~assets/AWS_UG_PTY.jpeg"
+          style="width: 800px"
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
